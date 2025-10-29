6de085a89a4194c2b2b8bfeb2690b15e0de0aedc
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-10-29 11:52:43 +0100

    feat: add initial project structure and configuration files

---
+++ builder.ts
@@ -0,0 +1,10 @@
+/**
+ * Builder - A tool that will build a new project
+ * Author: Jean-Philippe Maquestiaux
+ * License: EUPL-1.2
+ */
+
+console.log("Hello World from Builder! 🚀");
+
+// TODO: Add project building functionality
+console.log("Builder v0.0.0 - Ready to create amazing projects!");
